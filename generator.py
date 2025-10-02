06909e4c1e152f10655ed7bb86ce29f7cd90ce39
Zirui Cheng <ziruicheng01@gmail.com>
2025-10-02 14:42:52 +0000

    update belief updating

--- generator.py
+++ generator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, default="datasets/old_bailey.json")
     parser.add_argument("--limit", type=int, default=100)
     parser.add_argument("--prompt", type=str, default="prompts/law.yaml")
-    parser.add_argument("--model", type=str, default="meta-llama/llama-3.1-8b-instruct")
+    parser.add_argument("--model", type=str, default="meta-llama/llama-3.3-70b-instruct")
     parser.add_argument("--results", type=str, default="results")
     args = parser.parse_args()
 
@@ -24,14 +24,14 @@ if __name__ == "__main__":
     client = OpenRouterChatClient(api_key=os.getenv("OPENROUTER_API_KEY"))
 
     results = []
-    os.makedirs(os.path.dirname(args.output), exist_ok=True)
+    # os.makedirs(os.path.dirname(args.results), exist_ok=True)
 
     for i, item in enumerate(tqdm(dataset[:args.limit])):
         context = str(item["params"]["public"])
         information = str(item["params"]["private"])
 
         prompt = prompt_template.replace("<CONTEXT>", context).replace("<INFORMATION>", information)
-
+        
         response = client.create_completion(
             model=args.model,
             messages=[{"role": "user", "content": prompt}]
@@ -44,10 +44,13 @@ if __name__ == "__main__":
         result_item["response"] = response
         results.append(result_item)
 
+        model_name = args.model.split("/")[-1]
+        model_name = model_name.replace("-", "_")
+        
         # Save results every 10 items
         if (i + 1) % 10 == 0:
-            with open(args.results + f"/{args.model}_{args.limit}.json", "w") as f:
+            with open(args.results + f"/{model_name}_{args.limit}.json", "w") as f:
                 json.dump(results, f, indent=4)
 
-    with open(args.results + f"/{args.model}_{args.limit}.json", "w") as f:
+    with open(args.results + f"/{model_name}_{args.limit}.json", "w") as f:
         json.dump(results, f, indent=4)
